3d17f87bc857433ca239f8c70ffd3c907fdbd40b
zixin2006 <rowling.yu@icloud.com>
2024-10-27 21:18:37 +0800

    Site updated: 2024-10-27 21:18:36

--- js/page/post/index.js
+++
@@ -1,9 +0,0 @@
-import ImgPreview from "./img-preview.js";
-import { Toc } from "./aside.js";
-import Scroll from "../../utils/scroll.js";
-
-$(function () {
-  new Scroll();
-  new Toc();
-  new ImgPreview();
-});
